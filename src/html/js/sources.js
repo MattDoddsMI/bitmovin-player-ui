978180cd40d2e6cd7c14311839e8c6509680bafa
Matt Dodds <matthew.dodds@movingimage.com>
2018-07-24 09:16:57 +0200

    FIx Linter warnings on prod build

--- src/html/js/sources.js
+++ src/html/js/sources.js
@@ -1,12 +1,13 @@
 var sources = {
   live: {
     // hls: "https://m1w-fra.rec.vc/live/ngrp:hB7LUrYwxkOo2dwX_all/master.m3u8",
-    hls: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
+    // hls: "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8",
     // hls: "https://mi-msl4-amd.akamaized.net/hls/live/622389/MattStream_en/MattStream_en_all/playlist.m3u8",
-    // hls: "http://sample.vodobox.com/planete_interdite/planete_interdite_alternate.m3u8",
+    hls: "http://sample.vodobox.com/planete_interdite/planete_interdite_alternate.m3u8",
     // hls: "https://wbc-live-cdn-qa.edge-cdn.net/mi-webcast/ngrp:5ad9b56a0892378fab3f0f12_en_all/master.m3u8",
     // hls: "https://livesegmented-i.akamaihd.net/hls/live/240282/nginxTestStream/nginxTestStream/playlist.m3u8",
-    // hls: "https://livesegmented-i.akamaihd.net/hls/live/240282/5b31f587a0d5d301b840626a_en/5b31f587a0d5d301b840626a_en_all/playlist.m3u8",
+    // hls: "https://livesegmented-i.akamaihd.net/hls/live/240282/5b3ce706df9133000f24f382_en/5b3ce706df9133000f24f382_en_all/playlist.m3u8",    
+    // hls: "https://livesegmented-i.akamaihd.net/hls/live/240282/redundantWowzaTest/redundantWowzaTest/playlist.m3u8",
     title: "Live Source"
   },
   vod: {
